ee63dba5c4dff58a8ec277af06e0a09b1317f613
Donny Roufs <iamchets@yahoo.com>
2023-07-13 16:22:26 +0200

    undo debug prod

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -1,10 +1,3 @@
 import { ViewLoadouts } from "@bbforge/loadouts";
 
-// Figure out what native type is needed
-type PageProps = {
-  searchParams: {
-    filterBy?: string;
-  };
-};
-
 export default ViewLoadouts;
