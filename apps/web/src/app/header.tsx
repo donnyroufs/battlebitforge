e1df1750842c0b3950e18ec1b93f94cfdeeef076
Donny Roufs <iamchets@yahoo.com>
2023-07-13 19:39:05 +0200

    explicitly make link not shallow

--- apps/web/src/app/header.tsx
+++ apps/web/src/app/header.tsx
@@ -9,7 +9,7 @@ export function Header() {
       <header className="text-[#DAE2EB] justify-between items-center h-32 hidden md:flex">
         <div>
           <span className="uppercase mb-4 font-extrabold">v{version}</span>
-          <NextLink href="/">
+          <NextLink href="/" shallow={false}>
             <p className="text-3xl flex items-center uppercase font-extrabold">
               Battlebit Forge
             </p>
