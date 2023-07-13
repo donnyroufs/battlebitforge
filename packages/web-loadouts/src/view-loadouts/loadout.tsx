65634723797a8a784d2e5b1df5540b9c1911f129
Donny Roufs <iamchets@yahoo.com>
2023-07-13 12:49:40 +0200

    make loadout card clickable

--- packages/web-loadouts/src/view-loadouts/loadout.tsx
+++ packages/web-loadouts/src/view-loadouts/loadout.tsx
@@ -20,26 +20,26 @@ export function Loadout(props: GetLoadoutsResult[number]) {
       : `be the first to vote!`;
 
   return (
-    <div className="p-10 bg-[#212330]">
-      <Link href={`/loadouts/${props.slug}`}>
+    <Link href={`/loadouts/${props.slug}`}>
+      <div className="p-10 bg-[#212330]">
         <h2 className="text-2xl font-extrabold capitalize flex">
           {props.name}
         </h2>
-      </Link>
-      <Image
-        src="/ACR.png"
-        height={320}
-        width={320}
-        alt="weapon"
-        className="opacity-80 my-8 mx-auto"
-      />
+        <Image
+          src="/ACR.png"
+          height={320}
+          width={320}
+          alt="weapon"
+          className="opacity-80 my-8 mx-auto"
+        />
 
-      <div className="relative">
-        <Tooltip message={tooltipMessage}>
-          <LikeOMeter likes={totalLikes} total={totalVotes} />
-        </Tooltip>
+        <div className="relative">
+          <Tooltip message={tooltipMessage}>
+            <LikeOMeter likes={totalLikes} total={totalVotes} />
+          </Tooltip>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
