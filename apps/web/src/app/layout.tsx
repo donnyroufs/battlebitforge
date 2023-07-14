150d45fcb9d182fd5907c2dadf40f2c72ec3d146
Donny Roufs <iamchets@yahoo.com>
2023-07-14 12:56:09 +0200

    add meta tags

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -9,6 +9,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <title>Battlebit Forge</title>
+      <meta
+        name="description"
+        content="Forge and showcase your preferred loadouts for others to view and vote on. Effortlessly access analytics on top-performing weapons, attachments, and more."
+      />
+      <meta
+        name="keywords"
+        content="Battlebit, Battlebit Remastered, Loadouts, Share, Best Loadouts"
+      />
+      <meta name="author" content="Donny Roufs" />
       <NextAuthProvider>
         <body className="text-slate-300">
           <div className="container mx-auto mb-8 px-8 md:px-4">
